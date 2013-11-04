139a43688d9d9bc5541696143ebb4fee11600cc5
Andreas Lutro <anlutro@gmail.com>
2013-11-04 09:57:57 +0100

    refactor

--- src/c/CoreServiceProvider.php
+++ src/c/CoreServiceProvider.php
@@ -15,32 +15,53 @@ use Illuminate\Support\ServiceProvider;
 class CoreServiceProvider extends ServiceProvider
 {
 
+	protected $package;
+	protected $namespace;
+	protected $srcPath;
+
 	public function register()
 	{
-		$this->commands('c\Auth\Console\CreateUserCommand');
+		$this->commands([
+			'c\Auth\Console\CreateUserCommand',
+			'c\Auth\Console\ActivateUserCommand',
+			'c\Auth\Console\ChangePasswordCommand',
+			'c\Auth\Console\SendPasswordReminderCommand',
+		]);
 	}
 
 	public function boot()
 	{
-		$package = 'anlutro/l4-core';
-		$namespace = 'c';
-		$srcPath = __DIR__ . '/..';
+		$this->package = 'anlutro/l4-core';
+		$this->namespace = 'c';
+		$this->srcPath = __DIR__ . '/..';
 
-		$this->app['translator']->addNamespace($namespace, $srcPath . '/lang');
-
-		$appView = $this->getAppViewPath($package, $namespace);
-		
-		if ($this->app['files']->isDirectory($appView)) {
-			$this->app['view']->addNamespace($namespace, $appView);
-		}
-
-		$this->app['view']->addNamespace($namespace, $srcPath . '/views');
-
+		$this->registerLangFiles();
+		$this->registerViewFiles();
 		$this->addSidebarFunctionality();
-
-		require $srcPath . '/routes-' . $this->app['translator']->getLocale() . '.php';
+		$this->requireRouteFiles();
 
 		$this->app->bind('c\Auth\UserModel', $this->app['config']->get('auth.model', 'c\Auth\UserModel'));
+	}
+
+	protected function registerLangFiles()
+	{
+		$this->app['translator']->addNamespace($this->namespace, $this->srcPath . '/lang');
+	}
+
+	protected function registerViewFiles()
+	{
+		$appView = $this->getAppViewPath($this->package, $this->namespace);
+		
+		if ($this->app['files']->isDirectory($appView)) {
+			$this->app['view']->addNamespace($this->namespace, $appView);
+		}
+
+		$this->app['view']->addNamespace($this->namespace, $this->srcPath . '/views');
+	}
+
+	protected function requireRouteFiles()
+	{
+		require $this->srcPath . '/routes-' . $this->app['translator']->getLocale() . '.php';
 	}
 
 	protected function addSidebarFunctionality()
