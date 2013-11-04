0556039fe0698213ee50e47e571ac671a9c41640
Andreas Lutro <anlutro@gmail.com>
2013-11-04 09:58:01 +0100

    bugfix

--- src/c/Auth/Reminders/RemindableInterface.php
+++ src/c/Auth/Reminders/RemindableInterface.php
@@ -4,6 +4,6 @@ namespace c\Auth\Reminders;
 interface RemindableInterface
 {
 	public function getReminderEmail();
-	public function setPasswordAttribute();
+	public function setPasswordAttribute($password);
 	public function save();
 }
