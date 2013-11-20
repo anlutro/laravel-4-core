e707d5730c2818d95c26e5f3b9776168dc7828d4
Andreas Lutro <anlutro@gmail.com>
2013-11-20 09:42:26 +0100

    tests

---
+++ tests/controller/AuthControllerTest.php
@@ -0,0 +1,214 @@
+<?php
+use Mockery as m;
+use Illuminate\Support\Facades;
+
+class AuthControllerTest extends TestCase
+{
+	protected $controller = 'AuthController';
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->repo = m::mock('c\Auth\UserRepository');
+		$this->app->instance('c\Auth\UserRepository', $this->repo);
+	}
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	public function testLogin()
+	{
+		$this->getAction('login');
+
+		$this->assertResponseOk();
+	}
+
+	public function testLoginSuccess()
+	{
+		$input = ['username' => 'foo', 'password' => 'bar', 'baz' => 'bar'];
+		$credentials = array_only($input, ['username', 'password']);
+		Facades\Auth::shouldReceive('attempt')->once()
+			->with($credentials)->andReturn(true);
+
+		$this->postAction('login', [], $input);
+
+		$this->assertRedirectedTo('/');
+		$this->assertSessionHas('success');
+	}
+
+	public function testLoginFailure()
+	{
+		$input = ['username' => 'foo', 'password' => 'bar', 'baz' => 'bar'];
+		$credentials = array_only($input, ['username', 'password']);
+		Facades\Auth::shouldReceive('attempt')->once()
+			->with($credentials)
+			->andReturn(false);
+
+		$this->postAction('login', [], $input);
+
+		$this->assertRedirectedToAction('login');
+		$this->assertSessionHasErrors();
+	}
+
+	public function testLogout()
+	{
+		Facades\Auth::shouldReceive('logout')->once();
+
+		$this->getAction('logout');
+
+		$this->assertRedirectedToAction('login');
+	}
+
+	public function testResetStepOneForm()
+	{
+		$this->getAction('reminder');
+
+		$this->assertResponseOk();
+	}
+
+	public function testResetStepOneNotFound()
+	{
+		$input = ['email' => 'foo', 'bar' => 'baz'];
+		$credentials = array_only($input, ['email']);
+
+		$this->swapPasswordBrokerDependencies();
+		$this->repo->shouldReceive('getByCredentials')->once()
+			->with($credentials)->andReturn(false);
+
+		$this->postAction('sendReminder', [], $input);
+
+		$this->assertRedirectedToAction('reminder');
+		$this->assertSessionHasErrors();
+	}
+
+	public function testResetStepOneFailure()
+	{
+		$input = ['email' => 'foo', 'bar' => 'baz'];
+		$credentials = array_only($input, ['email']);
+		$mockUser = $this->getMockUser();
+
+		$this->swapPasswordBrokerDependencies();
+		$this->repo->shouldReceive('getByCredentials')->once()
+			->with($credentials)->andReturn($mockUser);
+		Facades\Password::shouldReceive('requestReset')->once()
+			->with($mockUser)->andReturn(false);
+
+		$this->postAction('sendReminder', [], $input);
+
+		$this->assertRedirectedToAction('reminder');
+		$this->assertSessionHasErrors();
+	}
+
+	public function testResetStepOneSuccess()
+	{
+		$input = ['email' => 'foo', 'bar' => 'baz'];
+		$credentials = array_only($input, ['email']);
+		$mockUser = $this->getMockUser();
+
+		$this->swapPasswordBrokerDependencies();
+		$this->repo->shouldReceive('getByCredentials')->once()
+			->with($credentials)->andReturn($mockUser);
+		Facades\Password::shouldReceive('requestReset')->once()
+			->with($mockUser)->andReturn(true);
+
+		$this->postAction('sendReminder', [], $input);
+
+		$this->assertRedirectedToAction('login');
+		$this->assertSessionHas('info');
+	}
+
+	public function testResetStepTwoForm()
+	{
+		$this->getAction('reset', [], ['token' => 'foobar']);
+
+		$this->assertResponseOk();
+	}
+
+	public function testResetStepTwoFormWithoutToken()
+	{
+		$this->getAction('reset', []);
+
+		$this->assertRedirectedToAction('login');
+	}
+
+	protected function setupResetExpectations(array $input, $result, $validates = true)
+	{
+		$credentials = array_only($input, ['username']);
+		$token = $input['token'];
+		$newPassword = $input['password'];
+		
+		Validator::shouldReceive('make')->once()
+			->andReturn(m::mock(['fails' => (!$validates)]));
+		if ($validates) {
+			$this->swapPasswordBrokerDependencies();
+			$user = $this->getMockUser();
+			$this->repo->shouldReceive('getByCredentials')->andReturn($user);
+			Facades\Password::shouldReceive('resetUser')->once()
+				->with($user, $token, $newPassword)
+				->andReturn($result);
+		}
+	}
+
+	public function testResetValidationFails()
+	{
+		$input = [
+			'username' => 'bar',
+			'token' => 'baz',
+			'password' => 'foo',
+			'password_confirm' => 'bar',
+			'foo' => 'baz'
+		];
+		$this->setupResetExpectations($input, false, false);
+
+		$this->postAction('attemptReset', [], $input);
+
+		$this->assertRedirectedTo('reset');
+		$this->assertSessionHasErrors();
+	}
+
+	public function testResetFailure()
+	{
+		$input = [
+			'username' => 'bar',
+			'token' => 'baz',
+			'password' => 'foo',
+			'password_confirm' => 'bar',
+			'foo' => 'baz'
+		];
+		$this->setupResetExpectations($input, false);
+
+		$this->postAction('attemptReset', [], $input);
+
+		$this->assertRedirectedTo('login');
+		$this->assertSessionHasErrors();
+	}
+
+	public function testResetSuccess()
+	{
+		$input = [
+			'username' => 'bar',
+			'token' => 'baz',
+			'password' => 'foo',
+			'password_confirm' => 'bar',
+			'foo' => 'baz'
+		];
+		$this->setupResetExpectations($input, true);
+
+		$this->postAction('attemptReset', [], $input);
+
+		$this->assertRedirectedTo('login');
+		$this->assertSessionHas('success');
+	}
+
+	public function swapPasswordBrokerDependencies()
+	{
+		$this->app['auth.reminder.repository'] = m::mock('Illuminate\Auth\Reminders\ReminderRepositoryInterface');
+	}
+
+	protected function getMockUser()
+	{
+		return m::mock('App\User')->makePartial();
+	}
+}
---
+++ tests/controller/UserControllerTest.php
@@ -0,0 +1,268 @@
+<?php
+use Mockery as m;
+
+class UserControllerTest extends TestCase
+{
+	protected $controller = 'UserController';
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->users = m::mock('c\Auth\UserRepository');
+		$this->app->instance('c\Auth\UserRepository', $this->users);
+	}
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	public function testViewProfile()
+	{
+		$curUser = $this->expectCurrentUser();
+
+		$this->getAction('profile');
+		$this->assertResponseOk();
+		$this->assertRouteHasFilter('auth');
+	}
+
+	public function testUpdateProfileWithIncorrectPassword()
+	{
+		$input = ['old_password' => 'foo'];
+		$curUser = $this->expectCurrentUser();
+		$curUser->shouldReceive('confirmPassword')
+			->with($input['old_password'])
+			->andReturn(false);
+
+		$this->postAction('updateProfile', [], $input);
+
+		$this->assertRedirectedToAction('profile');
+		$this->assertSessionHasErrors();
+	}
+
+	protected function setUpProfileUpdateExpectations($input, $result)
+	{
+		$curUser = $this->expectCurrentUser();
+		$curUser->shouldReceive('confirmPassword')
+			->andReturn(true);
+		$this->users->shouldReceive('updateProfile')
+			->with($curUser, $input)
+			->andReturn($result);
+	}
+
+	public function testUpdateProfileSuccess()
+	{
+		$input = ['foo' => 'bar'];
+		$this->setUpProfileUpdateExpectations($input, true);
+
+		$this->postAction('updateProfile', [], $input);
+
+		$this->assertRedirectedToAction('profile');
+		$this->assertSessionHas('success');
+	}
+
+	public function testUpdateProfileFailure()
+	{
+		$input = ['foo' => 'bar'];
+		$this->setUpProfileUpdateExpectations($input, false);
+		$this->users->shouldReceive('errors')
+			->andReturn(['baz' => 'bar']);
+
+		$this->postAction('updateProfile', [], $input);
+
+		$this->assertRedirectedToAction('profile');
+		$this->assertSessionHasErrors('baz');
+	}
+
+	protected function setUpIndexExpectations()
+	{
+		$this->users->shouldReceive('togglePagination')->once();
+		$this->users->shouldReceive('getUserTypes')->once()
+			->andReturn([]);
+		$this->users->shouldReceive('getAll')->once()
+			->andReturn([]);
+	}
+
+	public function testIndex()
+	{
+		$this->setUpIndexExpectations();
+
+		$this->getAction('index');
+
+		$this->assertResponseOk();
+		$this->assertRouteHasFilter('auth');
+		$this->assertRouteHasFilter('access:admin');
+		$this->assertViewHas('users');
+	}
+
+	public function testIndexWithSearch()
+	{
+		$this->setUpIndexExpectations();
+		$this->users->shouldReceive('search')
+			->with('foo');
+
+		$this->getAction('index', ['search' => 'foo']);
+
+		$this->assertResponseOk();
+	}
+
+	public function testIndexWithFilter()
+	{
+		$this->setUpIndexExpectations();
+		$this->users->shouldReceive('filter')
+			->with('bar');
+
+		$this->getAction('index', ['usertype' => 'bar']);
+
+		$this->assertResponseOk();
+	}
+
+	public function testBulkAction()
+	{
+		$ids = [1 => 'checked', 2 => 'checked', 3 => 'checked'];
+		$input = ['bulkAction' => 'delete', 'bulk' => $ids];
+		$this->users->shouldReceive('processBulkAction')->once()
+			->with($input['bulkAction'], array_keys($ids));
+
+		$this->postAction('bulk', [], $input);
+
+		$this->assertRedirectedToAction('index');
+	}
+
+	public function testNotFound()
+	{
+		$this->users->shouldReceive('getByKey')->andReturn(false);
+
+		$this->getAction('show', [1]);
+
+		$this->assertRedirectedToAction('index');
+		$this->assertSessionHasErrors();
+	}
+
+	protected function expectFindUser($id)
+	{
+		$mockUser = $this->getMockUser();
+		$mockUser->id = $id;
+		$this->users->shouldReceive('getByKey')->once()->andReturn($mockUser);
+		return $mockUser;
+	}
+
+	public function testShow()
+	{
+		$id = 1; $user = $this->expectFindUser($id);
+		$curUser = $this->expectCurrentUser();
+		$curUser->shouldReceive('hasAccess')->with('admin')->andReturn(true);
+
+		$this->getAction('show', [$id]);
+
+		$this->assertResponseOk();
+		$this->assertViewHas('user', $user);
+		$this->assertViewHas('editUrl');
+	}
+
+	public function testEdit()
+	{
+		$id = 1; $user = $this->expectFindUser($id);
+		$this->users->shouldReceive('getUserTypes')->andReturn([]);
+
+		$this->getAction('edit', [$id]);
+
+		$this->assertResponseOk();
+		$this->assertViewHas('user', $user);
+	}
+
+	protected function setupUpdateExpectation($input, $id, $result)
+	{
+		$user = $this->expectFindUser($id);
+		$this->users->shouldReceive('update')->once()
+			->with($user, $input)
+			->andReturn($result);
+		return $user;
+	}
+
+	public function testUpdateSuccess()
+	{
+		$input = ['foo' => 'bar']; $id = 1;
+		$this->setupUpdateExpectation($input, $id, true);
+
+		$this->postAction('update', [$id], $input);
+
+		$this->assertRedirectedToAction('edit', [$id]);
+		$this->assertSessionHas('success');
+	}
+
+	public function testUpdateFailure()
+	{
+		$input = ['foo' => 'bar']; $id = 1;
+		$this->setupUpdateExpectation($input, $id, false);
+		$this->users->shouldReceive('errors')
+			->andReturn(['baz' => 'bar']);
+
+		$this->postAction('update', [$id], $input);
+
+		$this->assertRedirectedToAction('edit', [$id]);
+		$this->assertSessionHasErrors();
+	}
+
+	public function testCreate()
+	{
+		$this->users->shouldReceive('getNew')->once()
+			->andReturn($this->getMockUser());
+
+		$this->getAction('create');
+
+		$this->assertResponseOk();
+	}
+
+	public function testStoreSuccess()
+	{
+		$input = ['foo' => 'bar'];
+		$user = $this->getMockUser(); $user->id = 1;
+		$this->users->shouldReceive('create')->once()
+			->with($input, false)->andReturn($user);
+
+		$this->postAction('store', [], $input);
+
+		$this->assertRedirectedToAction('edit', [$user->id]);
+		$this->assertSessionHas('success');
+	}
+
+	public function testStoreActivateSuccess()
+	{
+		$input = ['foo' => 'bar', 'activate' => '1'];
+		$user = $this->getMockUser(); $user->id = 1;
+		$this->users->shouldReceive('create')->once()
+			->with($input, true)->andReturn($user);
+
+		$this->postAction('store', [], $input);
+
+		$this->assertRedirectedToAction('edit', [$user->id]);
+		$this->assertSessionHas('success');
+	}
+
+	public function testStoreFailure()
+	{
+		$input = ['foo' => 'bar'];
+		$this->users->shouldReceive('create')->once()
+			->with($input, m::any())->andReturn(false);
+		$this->users->shouldReceive('errors')->once()
+			->andReturn('baz');
+
+		$this->postAction('store', [], $input);
+
+		$this->assertRedirectedToAction('create');
+		$this->assertSessionHasErrors();
+	}
+
+	protected function expectCurrentUser()
+	{
+		$mockUser = $this->getMockUser();
+		$this->users->shouldReceive('getCurrentUser')->andReturn($mockUser);
+		return $mockUser;
+	}
+
+	protected function getMockUser()
+	{
+		return m::mock('App\User')->makePartial();
+	}
+}
