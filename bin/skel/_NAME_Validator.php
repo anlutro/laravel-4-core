de884ef4104d396e44df1133c5d556334ca05d67
Andreas Lutro <anlutro@gmail.com>
2014-01-09 10:37:00 +0100

    fix validator class

--- bin/skel/_NAME_Validator.php
+++ bin/skel/_NAME_Validator.php
@@ -6,12 +6,26 @@ namespace _NAMESPACE_;
  */
 class _NAME_Validator extends \c\Validator
 {
+	public function __construct(_NAME_Model $model)
+	{
+		parent::__construct($model);
+	}
+
+	/**
+	 * Common rules are ran on every validation call.
+	 */
 	protected $commonRules = [
 	];
 
+	/**
+	 * Create rules are only ran when creating a new model.
+	 */
 	protected $createRules = [
 	];
 
+	/**
+	 * Update rules are only ran when updating an existing model.
+	 */
 	protected $updateRules = [
 	];
 }
