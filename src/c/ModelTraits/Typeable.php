42a3efedcbf5f0dc4355c35588882c11cafc42d5
Andreas Lutro <anlutro@gmail.com>
2014-02-20 10:52:19 +0100

    add Typeable trait

---
+++ src/c/ModelTraits/Typeable.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Laravel 4 Core
+ *
+ * @author   Andreas Lutro <anlutro@gmail.com>
+ * @license  http://opensource.org/licenses/MIT
+ * @package  l4-core
+ */
+
+namespace c\ModelTraits;
+
+/**
+ * Generic traits for models that store their type as an integer but also needs
+ * them to be represented as a string.
+ * 
+ * Classes that implement this trait must have a protected static $types assoc
+ * array of int => string values.
+ */
+trait Typeable
+{
+	/**
+	 * Getter for $model->type. Returns the string representation.
+	 */
+	public function getTypeAttribute($value)
+	{
+		if ($value !== null) return $this->getTypeName($value);
+	}
+
+	/**
+	 * Setter for $model->type
+	 */
+	public function setTypeAttribute($value)
+	{
+		$this->attributes['type'] = $this->getTypeValue($value);
+	}
+
+	/**
+	 * Getter for $model->type_value - returns the raw int value.
+	 */
+	public function getTypeValueAttribute()
+	{
+		return $this->getAttributeFromArray('type');
+	}
+
+	/**
+	 * Get the string representation for a given type.
+	 *
+	 * @param  string|int $type
+	 *
+	 * @return string
+	 */
+	public function getTypeName($type)
+	{
+		if (is_numeric($type)) {
+			if (!isset(static::$types[$type])) {
+				throw new \InvalidArgumentException("Unknown type: $type");
+			}
+			return static::$types[$type];
+		} else {
+			$type = strtolower($type);
+			$types = array_flip(static::$types);
+			if (!isset($types[$type])) {
+				throw new \InvalidArgumentException("Unknown type: $type");
+			}
+			return $type;
+		}
+	}
+
+	/**
+	 * Get the int representation for a given type.
+	 *
+	 * @param  string|int $type
+	 *
+	 * @return int
+	 */
+	public function getTypeValue($type)
+	{
+		if (is_numeric($type)) {
+			if (!isset(static::$types[$type])) {
+				throw new \InvalidArgumentException("Unknown type: $type");
+			}
+			return $type;
+		} else {
+			$type = strtolower($type);
+			$types = array_flip(static::$types);
+			if (!isset($types[$type])) {
+				throw new \InvalidArgumentException("Unknown type: $type");
+			}
+			return $types[$type];
+		}
+	}
+
+	/**
+	 * Get all available types.
+	 *
+	 * @return array
+	 */
+	public function getAvailableTypes()
+	{
+		return static::$types;
+	}
+
+	/**
+	 * Add $query->whereType(...) functionality.
+	 *
+	 * @param  Illuminate\Database\Eloquent\Builder $query
+	 * @param  string|int $filter
+	 *
+	 * @return Illuminate\Database\Eloquent\Builder
+	 */
+	public function scopeFilterType($query, $filter)
+	{
+		return $query->where('type', '=', $this->getTypeValue($filter));
+	}
+}
