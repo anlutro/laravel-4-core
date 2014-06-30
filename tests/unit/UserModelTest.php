2adf3fccd29ef67b44ba0fdb047c86f5a0bd11f4
Andreas Lutro <anlutro@gmail.com>
2014-06-30 21:42:56 +0200

    test allowed types

--- tests/unit/UserModelTest.php
+++ tests/unit/UserModelTest.php
@@ -9,13 +9,16 @@ class UserModelTest extends PHPUnit_Framework_TestCase
 	/**
 	 * @dataProvider getUserLevelData
 	 */
-	public function testUserLevels($level, $type, $denied = array())
+	public function testUserLevels($level, $type, array $allowed, array $denied)
 	{
 		$user = $this->makeUserModel();
 		$user->user_level = $level;
 		$this->assertEquals($type, $user->user_type);
 		$this->assertTrue($user->hasAccess($type));
 
+		foreach ($allowed as $type) {
+			$this->assertTrue($user->hasAccess($type));
+		}
 		foreach ($denied as $type) {
 			$this->assertFalse($user->hasAccess($type));
 		}
@@ -24,16 +27,16 @@ class UserModelTest extends PHPUnit_Framework_TestCase
 	public function getUserLevelData()
 	{
 		return [
-			[1, 'user', ['mod', 'admin', 'superadmin']],
-			[2, 'user', ['mod', 'admin', 'superadmin']],
-			[9, 'user', ['mod', 'admin', 'superadmin']],
-			[10, 'mod', ['admin', 'superadmin']],
-			[11, 'mod', ['admin', 'superadmin']],
-			[99, 'mod', ['admin', 'superadmin']],
-			[100, 'admin', ['superadmin']],
-			[101, 'admin', ['superadmin']],
-			[254, 'admin', ['superadmin']],
-			[255, 'superadmin', []],
+			[1, 'user', [], ['mod', 'admin', 'superadmin']],
+			[2, 'user', [], ['mod', 'admin', 'superadmin']],
+			[9, 'user', [], ['mod', 'admin', 'superadmin']],
+			[10, 'mod', ['mod'], ['admin', 'superadmin']],
+			[11, 'mod', ['mod'], ['admin', 'superadmin']],
+			[99, 'mod', ['mod'], ['admin', 'superadmin']],
+			[100, 'admin', ['mod', 'admin'], ['superadmin']],
+			[101, 'admin', ['mod', 'admin'], ['superadmin']],
+			[254, 'admin', ['mod', 'admin'], ['superadmin']],
+			[255, 'superadmin', ['mod', 'admin', 'superadmin'], []],
 		];
 	}
 
