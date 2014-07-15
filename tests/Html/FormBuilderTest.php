52ccd2e7fdb94b59abddc503909c48b20b47b347
Andreas Lutro <anlutro@gmail.com>
2014-07-15 11:55:24 +0200

    add a test and slight refactor

--- tests/Html/FormBuilderTest.php
+++ tests/Html/FormBuilderTest.php
@@ -10,11 +10,13 @@ use anlutro\Core\Html\FormBuilder;
 
 class FormBuilderTest extends PHPUnit_Framework_TestCase
 {
-	protected function makeFormBuilder()
+	protected function makeFormBuilder($model = null)
 	{
 		$urlGenerator = new UrlGenerator(new RouteCollection, Request::create('/foo', 'GET'));
 		$htmlBuilder = new HtmlBuilder($urlGenerator);
-		return new FormBuilder($htmlBuilder, $urlGenerator, '');
+		$formBuilder = new FormBuilder($htmlBuilder, $urlGenerator, '');
+		if ($model) $formBuilder->setModel($model);
+		return $formBuilder;
 	}
 
 	/** @test */
@@ -23,10 +25,22 @@ class FormBuilderTest extends PHPUnit_Framework_TestCase
 		$obj = new \StdClass;
 		$obj->stuff = new \Illuminate\Support\Collection([5 => ['bar' => 'baz']]);
 		$model = ['foo' => $obj];
-		$form = $this->makeFormBuilder();
-		$form->setModel($model);
+		$form = $this->makeFormBuilder($model);
 		$this->assertEquals('baz', $form->value('foo[stuff][5][bar]'));
 		$this->assertContains('value="baz"', $form->text('foo[stuff][5][bar]'));
+	}
+
+	/** @test */
+	public function getsEloquentObjectsProperly()
+	{
+		$model = new \StdClass;
+		$related = [new StubModel(['id' => 1, 'name' => 'foo']), new StubModel(['id' => 2, 'name' => 'bar'])];
+		$model->related = new \Illuminate\Database\Eloquent\Collection($related);
+		$form = $this->makeFormBuilder($model);
+		$this->assertEquals(null, $form->value('related[0][name]'));
+		$this->assertEquals('foo', $form->value('related[1][name]'));
+		$this->assertEquals('bar', $form->value('related[2][name]'));
+		$this->assertEquals(null, $form->value('related[3][name]'));
 	}
 
 	/** @test */
@@ -44,4 +58,9 @@ class FormBuilderTest extends PHPUnit_Framework_TestCase
 		$this->assertNotContains('checked', $form->checkbox('foo', 1, false));
 		$this->assertContains('checked', $form->checkbox('foo', 1, true));
 	}
+}
+
+class StubModel extends \Illuminate\Database\Eloquent\Model
+{
+	protected $guarded = [];
 }
