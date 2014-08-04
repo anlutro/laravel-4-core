ac14822cffb3746ab974cf0d0b1134f640820324
Andreas Lutro <anlutro@gmail.com>
2014-08-04 08:36:00 +0200

    add linebreaks to support email

--- resources/views/support/mail.blade.php
+++ resources/views/support/mail.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 
 <p>
-	{{ trans('c::support.subject-label') }}: {{{ $subject }}}
-	{{ trans('c::support.email-label') }}: <a href="mailto:{{ $email }}">{{{ $email }}}</a>
-	{{ trans('c::support.phone-label') }}: {{{ $phone }}}
+	{{ trans('c::support.subject-label') }}: {{{ $subject }}}<br>
+	{{ trans('c::support.email-label') }}: <a href="mailto:{{ $email }}">{{{ $email }}}</a><br>
+	{{ trans('c::support.phone-label') }}: {{{ $phone }}}<br>
 	{{ ucfirst(trans('c::user.model-user')) }}: <a href="{{ route('c::user.edit', [$user->id]) }}">{{{ $user->username }}}</a>
 </p>
 <p>
