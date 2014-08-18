7e6d8c6cf6801d39d7b86af7a0032b6df473c475
Andreas Lutro <anlutro@gmail.com>
2014-08-18 14:55:29 +0200

    add docblock [ci skip]

--- src/Auth/Reminders/ReminderException.php
+++ src/Auth/Reminders/ReminderException.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Laravel 4 Core
+ *
+ * @author   Andreas Lutro <anlutro@gmail.com>
+ * @license  http://opensource.org/licenses/MIT
+ * @package  l4-core
+ */
+
 namespace anlutro\Core\Auth\Reminders;
 
 class ReminderException extends \Exception {}
