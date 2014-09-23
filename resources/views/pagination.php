7b99e86803662b19c6a17c3dbe890ceb2944c8b1
Andreas Lutro <anlutro@gmail.com>
2014-09-23 08:20:43 +0200

    newlines at the end of view files

--- resources/views/pagination.php
+++ resources/views/pagination.php
@@ -1,5 +1,5 @@
 <?php if ($paginator->getLastPage() > 1): ?>
 	<ul class="pagination">
-			<?php echo (new anlutro\Core\Html\PaginationPresenter($paginator))->render(); ?>
+		<?php echo (new anlutro\Core\Html\PaginationPresenter($paginator))->render(); ?>
 	</ul>
 <?php endif; ?>
